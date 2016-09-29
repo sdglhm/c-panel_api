6a4f381b92041c3370dc0b160eab71876e1e3cd4
Lahiru Himesh Madusanka <lahiru.lahirumadusanka@gmail.com>
2016-09-29 17:17:22 +0000

    Applied fixes from StyleCI

--- src/eezpal/cPanel/cPanel.php
+++ src/eezpal/cPanel/cPanel.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 
 // Copyright (c) 2016 Lahiru Himesh
 
@@ -20,135 +21,145 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-	 
-	/**
-	 * Better documentation added !
-	 */
-	namespace eezpal\cPanel_api;
 
-	/**
-	 * cPanel Class for handling
-	 */
-	class cPanel
-	{
-		
-		private $host;
-		private $username;
-		private $hash;
-		protected $headers=array();
+    /**
+     * Better documentation added !
+     */
+namespace eezpal\cPanel_api;
 
-		/**
-		 * [__construct Constructing cPanel api connection]
-		 * @param array $options Passing user, host name and access hash for cPanel
-		 */
-		public function __construct($options=array()){
-			return $this->checkSettings($options)
-				->setHost($options['host'])
-				->setAuth($options['user'], $options['hash'])
-			;
-		}
+/**
+     * cPanel Class for handling.
+     */
+    class cPanel
+    {
+        private $host;
+        private $username;
+        private $hash;
+        protected $headers = [];
 
-		/**
-		 * [__call Method invoking __call will build passed arguments]
-		 * @param  [type] $method [description]
-		 * @param  [type] $arg    [description]
-		 * @return [type]         [description]
-		 */
-		public function __call($method, $arg)
-		{
-			$this->buildArg($arg['0']);
-		    return $this->cpQuery($method);
-		}
+        /**
+         * [__construct Constructing cPanel api connection].
+         *
+         * @param array $options Passing user, host name and access hash for cPanel
+         */
+        public function __construct($options = [])
+        {
+            return $this->checkSettings($options)
+                ->setHost($options['host'])
+                ->setAuth($options['user'], $options['hash']);
+        }
 
-		/**
-		 * Check if settings are present with the passed variables
-		 * @param  [] $options [description]
-		 * @return [type]          [description]
-		 */
-		private function checkSettings($options)
-		{
-		    if(empty($options['user']))
-		      throw new \Exception('Username is not set', 2301);
-		    if(empty($options['hash']))
-		      throw new \Exception('Hash is not set', 2302);
-		    if(empty($options['host']))
-		      throw new \Exception('CPanel Host is not set', 2303);
-		    return $this;
-		}
+        /**
+         * [__call Method invoking __call will build passed arguments].
+         *
+         * @param [type] $method [description]
+         * @param [type] $arg    [description]
+         *
+         * @return [type] [description]
+         */
+        public function __call($method, $arg)
+        {
+            $this->buildArg($arg['0']);
 
-		
+            return $this->cpQuery($method);
+        }
 
-		public function setHost($host)
-		{
-		    $this->host = $host;
-		    return $this;
-		}
+        /**
+         * Check if settings are present with the passed variables.
+         *
+         * @param [] $options [description]
+         *
+         * @return [type] [description]
+         */
+        private function checkSettings($options)
+        {
+            if (empty($options['user'])) {
+                throw new \Exception('Username is not set', 2301);
+            }
+            if (empty($options['hash'])) {
+                throw new \Exception('Hash is not set', 2302);
+            }
+            if (empty($options['host'])) {
+                throw new \Exception('CPanel Host is not set', 2303);
+            }
 
-		public function setAuth($user, $hash)
-		{
-		    $this->user = $user;
-		    $this->hash = $hash;
-		    return $this;
-		}
+            return $this;
+        }
 
-		public function callHost()
-		{
-			return $this->host;
-		}
+        public function setHost($host)
+        {
+            $this->host = $host;
 
-		public function callUser()
-		{
-			return $this->user;
-		}
+            return $this;
+        }
 
-		public function callHash()
-		{
-			return $this->hash;
-		}
+        public function setAuth($user, $hash)
+        {
+            $this->user = $user;
+            $this->hash = $hash;
 
-		public function setHeader($name, $value='')
-		{
-		    $this->headers[$name] = $value;
-		    return $this;
-		}
+            return $this;
+        }
 
-		public function buildArg($arg){
-			$this->arg = http_build_query($arg);
-			return $this;
-		}
+        public function callHost()
+        {
+            return $this->host;
+        }
 
-		private function makeHeader()
-		{
-			$headers = $this->headers;
-			$user = $this->callUser();
-			$hash = $this->callHash();
+        public function callUser()
+        {
+            return $this->user;
+        }
 
-			return $headers['Authorization'] = 'WHM ' . $user . ':' . preg_replace("'(\r|\n)'","",$hash);
-		}
+        public function callHash()
+        {
+            return $this->hash;
+        }
 
-		protected function cpQuery($method)
-		{
-			$host = $this->callHost();
-			$user = $this->callUser();
-			$hash = $this->callHash();
-			$arg = $this->arg;
-			$query = $host . ':2087/json-api/' . $method . '?api.version=1&'. $arg ;
+        public function setHeader($name, $value = '')
+        {
+            $this->headers[$name] = $value;
 
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,0);
-			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,0);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
-			  
-			$header[0] = 'Authorization: WHM ' . $user . ':' . preg_replace("'(\r|\n)'","",$hash);
+            return $this;
+        }
 
-			curl_setopt($curl,CURLOPT_HTTPHEADER,$header);
-			curl_setopt($curl, CURLOPT_URL, $query);
-			  
-			$result = curl_exec($curl);
-			curl_close($curl);
+        public function buildArg($arg)
+        {
+            $this->arg = http_build_query($arg);
 
-		return $result;
-		}
-	}
+            return $this;
+        }
 
- ?>
+        private function makeHeader()
+        {
+            $headers = $this->headers;
+            $user = $this->callUser();
+            $hash = $this->callHash();
+
+            return $headers['Authorization'] = 'WHM '.$user.':'.preg_replace("'(\r|\n)'", '', $hash);
+        }
+
+        protected function cpQuery($method)
+        {
+            $host = $this->callHost();
+            $user = $this->callUser();
+            $hash = $this->callHash();
+            $arg = $this->arg;
+            $query = $host.':2087/json-api/'.$method.'?api.version=1&'.$arg;
+
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+            $header[0] = 'Authorization: WHM '.$user.':'.preg_replace("'(\r|\n)'", '', $hash);
+
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($curl, CURLOPT_URL, $query);
+
+            $result = curl_exec($curl);
+            curl_close($curl);
+
+            return $result;
+        }
+    }
